dfe15e2154c5c87429d935a4cf03aeada0d075ad
Oskar Thoren <ot@oskarthoren.com>
2021-11-09 12:53:58 +0800

    Debug

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use ark_groth16::{
     create_random_proof as prove, generate_random_parameters, prepare_verifying_key, verify_proof,
 };
 
-// Include ark circom for hello world
 fn main() {
-    println!("Hello, world!");
+    println!("Load WASM and R1CS for witness and proof generation");
 
     // Load the WASM and R1CS for witness and proof generation
     let cfg = CircomConfig::<Bn254>::new(
@@ -17,25 +16,37 @@ fn main() {
         "./circuits/multiplier2.r1cs",
     ).unwrap();
 
+    println!("Build public input config");
+
     // Insert our public inputs as key value pairs
     let mut builder = CircomBuilder::new(cfg);
     builder.push_input("a", 3);
     builder.push_input("b", 11);
 
+    println!("Create setup");
+
     // Create an empty instance for setting it up
     let circom = builder.setup();
+
+    println!("Run trusted setup");
 
     // Run a trusted setup
     let mut rng = thread_rng();
     let params = generate_random_parameters::<Bn254, _, _>(circom, &mut rng).unwrap();
+
+    println!("Get circuit with witness");
 
     // Get the populated instance of the circuit with the witness
     let circom = builder.build().unwrap();
 
     let inputs = circom.get_public_inputs().unwrap();
 
+    println!("Generate proof");
+
     // Generate the proof
     let proof = prove(circom, &params, &mut rng).unwrap();
+
+    println!("Validate proof");
 
     // Check that the proof is valid
     let pvk = prepare_verifying_key(&params.vk);
